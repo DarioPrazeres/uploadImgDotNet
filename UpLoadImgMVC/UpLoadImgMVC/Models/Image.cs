e6156fd0f92b9564d5e96ad543a11ce505d0239a
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-07-25 23:39:53 +0100

    uploading first

---
+++ UpLoadImgMVC/UpLoadImgMVC/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UpLoadImgMVC.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageColum { get; set; }
+    }
+}
